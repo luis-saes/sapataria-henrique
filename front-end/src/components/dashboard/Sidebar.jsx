1f4f8da2705ba25abd17082341074e97f593a294
Luis Saes <saeslhs@gmail.com>
2021-11-04 19:53:23 -0300

    Estilizando barra lateral dashboard

--- front-end/src/components/dashboard/Sidebar.jsx
+++ front-end/src/components/dashboard/Sidebar.jsx
@@ -1,7 +1,46 @@
 import React from "react";
+import SectionButton from "./SectionButton";
+import { ReactComponent as Home } from "../../assets/icons/home.svg";
+import { ReactComponent as Worker } from "../../assets/icons/worker.svg";
+import { ReactComponent as Client } from "../../assets/icons/client.svg";
+import { ReactComponent as Shoes } from "../../assets/icons/shoes.svg";
+import { ReactComponent as Sales } from "../../assets/icons/sales.svg";
+import { ReactComponent as Logout } from "../../assets/icons/logout.svg";
 
 const Sidebar = () => {
-  return <div></div>;
+  const currentPathName = window.location.pathname;
+  return (
+    <div>
+      <div style={{ marginTop: "20px" }}>
+        <SectionButton
+          logo={<Home />}
+          text="Início"
+          active={currentPathName === "/dashboard"}
+        />
+      </div>
+      <SectionButton
+        logo={<Worker />}
+        text="Funcionários"
+        active={currentPathName === "/funcionarios"}
+      />
+      <SectionButton
+        logo={<Client />}
+        text="Clientes"
+        active={currentPathName === "/clientes"}
+      />
+      <SectionButton
+        logo={<Shoes />}
+        text="Sapatos"
+        active={currentPathName === "/sapatos"}
+      />
+      <SectionButton
+        logo={<Sales />}
+        text="Vendas"
+        active={currentPathName === "/vendas"}
+      />
+      <SectionButton logo={<Logout />} text="Sair" />
+    </div>
+  );
 };
 
 export default Sidebar;
