8f93156bebc72dcd3b99a4e7511c9d070e7e9ccc
Luis Saes <saeslhs@gmail.com>
2021-11-29 11:47:37 -0300

    Adicionando modal de erro ao formulario

--- front-end/src/components/dashboard/funcionarios/Form.jsx
+++ front-end/src/components/dashboard/funcionarios/Form.jsx
@@ -3,11 +3,25 @@ import { Link } from "react-router-dom";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
 import Sidebar from "../Sidebar";
 import { useForm, Form } from "../generic/useForm";
 import Input from "../generic/Input";
 import styles from "../Dashboard.module.css";
 import { validate } from "gerador-validador-cpf";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const initialFValues = {
   cpf: "",
@@ -88,7 +102,9 @@ const Funcionarios = () => {
 
   const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
     useForm(initialFValues, true, validate);
-  const [post, setPost] = React.useState(null);
+  const [modalText, setModalText] = useState("");
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
 
   const submitHandler = (event) => {
     event.preventDefault();
@@ -103,8 +119,8 @@ const Funcionarios = () => {
           body: JSON.stringify(values),
         });
         const content = await rawResponse.json();
-        console.log(1234567890);
-        console.log(content);
+        setOpen(true);
+        setModalText(content.error.message);
       })();
     } else {
       console.log("Deu ruim!");
@@ -114,6 +130,21 @@ const Funcionarios = () => {
   return (
     <div className={styles.dashboardOther}>
       <Sidebar />
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Erro na inserção.
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {`Descrição: ${modalText}`}
+          </Typography>
+        </Box>
+      </Modal>
       <Form onSubmit={submitHandler}>
         <Box
           sx={{
