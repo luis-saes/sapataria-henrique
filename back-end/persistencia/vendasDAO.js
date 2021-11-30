9e143b13abd3497a8cd5caa9f7fa33b3bad81a0f
Luis Saes <saeslhs@gmail.com>
2021-11-29 21:48:49 -0300

    Alterando base vendasDAO para comportar multiplas tabelas

--- back-end/persistencia/vendasDAO.js
+++ back-end/persistencia/vendasDAO.js
@@ -16,14 +16,30 @@ module.exports = class VendasDAO {
       return false;
     }
     console.log("Deu bom na inserção!!!!");
+    console.log(
+      db.execute(`SELECT * FROM cliente WHERE CPF = '${jsonObj.cpfCliente}'`)
+    );
+    console.log(
+      db.execute(
+        `SELECT * FROM funcionario WHERE CPF = '${jsonObj.cpfFuncionario}'`
+      )
+    );
     return db.execute(
-      `INSERT INTO venda (ID, Situacao, Data, FormaPagamento, PrecoTotal, CPFFuncionario, CPFCliente) VALUES (${id}, '${situacao}', ${data}, '${formaPagamento}', ${precoTotal}, '${CPFFuncionario}', '${CPFCliente}')`
+      `INSERT INTO venda (ID, Situacao, Data, FormaPagamento, PrecoTotal, CPFFuncionario, CPFCliente) VALUES (${id}, '${situacao}', ${data}, '${formaPagamento}', ${precoTotal}, '${cpfFuncionario}', '${cpfCliente}')`
     );
   }
 
   static updateElement(jsonObj) {
     jsonObj.precoTotal = vendasModel.changeToPrecision2(
       Number(jsonObj.precoTotal)
+    );
+    console.log(
+      db.execute(`SELECT * FROM cliente WHERE CPF = '${jsonObj.cpfCliente}'`)
+    );
+    console.log(
+      db.execute(
+        `SELECT * FROM funcionario WHERE CPF = '${jsonObj.cpfFuncionario}'`
+      )
     );
     return db.execute(
       `UPDATE venda SET ID = ${id}, Situacao = '${situacao}', Data = ${data}, FormaPagamento = '${formaPagamento}', PrecoTotal = ${precoTotal}, CPFFuncionario = '${CPFFuncionario}', CPFCliente ='${CPFCliente} WHERE ID = '${jsonObj.ID}'`
